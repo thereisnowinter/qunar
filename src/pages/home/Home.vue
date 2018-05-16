604d0c260b94f4c88d1a97224c8b897dfaac8a24
thereisnowinter <505975669@qq.com>
2018-05-17 00:13:48 +0800

    start project

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
-    hello world
-  </div>
+  <home-header></home-header>
 </template>
 
+
 <script>
+  import HomeHeader from './components/Header';
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      HomeHeader
+    }
   }
+
 </script>
 
 <style>
-  
+
+
 </style>
