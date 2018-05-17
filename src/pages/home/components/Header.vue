6858ba057db362cd9e6e32f9d34a09f67f7f8815
thereisnowinter <505975669@qq.com>
2018-05-17 14:08:57 +0800

    stylus bug fixed

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -22,33 +22,33 @@
 </script>
 
 <style lang="stylus" scoped>
-    /* @import '~styles/varibles.styl' */
-    .header
-      display: flex
-      line-height: .86rem
-      background: $bgColor
-      color: #fff
-      .header-left
-        width: .64rem
-        float: left
-        .back-icon
-          text-align: center
-          font-size: .4rem
-      .header-input
-        flex: 1
-        height: .64rem
-        line-height: .64rem
-        margin-top: .12rem
-        margin-left: .2rem
-        padding-left: .2rem
-        background: #fff
-        border-radius: .1rem
-        color: #ccc
-      .header-right
-        width: 1.24rem
-        float: right
+  @import '~styles/variables.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
         text-align: center
-        .arrow-icon
-          margin-left: -.04rem
-          font-size: .24rem
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
 </style>
