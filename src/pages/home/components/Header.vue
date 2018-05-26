19a773672928ba723f922546eff1a9cae5600873
thereisnowinter <505975669@qq.com>
2018-05-26 21:28:15 +0800

    finished page

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -9,7 +9,7 @@
     </div>
     <router-link to='/city'>
       <div class="header-right">
-        {{city}}
+        {{this.city}}
         <span class="iconfont arrow-icon">&#xe64a;</span>
       </div>
     </router-link>
@@ -17,14 +17,11 @@
 </template>
 
 <script>
-import { mapState ,mapGetters } from 'vuex'
+import { mapState } from 'vuex'
 export default {
   name: 'HomeHeader',
   computed: {
-    ...mapState([
-    'city'
-    ]),
-    ...mapGetters(['doubleCity'])
+    ...mapState(['city'])
   }
 }
 </script>
